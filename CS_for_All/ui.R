eafdde84df283a27256043f48536ff0f36356d05
Michelle <colaamigo@hotmail.com>
2026-02-03 14:42:59 -0800

    Updated Shiny structure

--- CS_for_All/ui.R
+++ CS_for_All/ui.R
@@ -8,15 +8,21 @@ ui <- fluidPage(
     navset_pill(
       id = "tab",
       
-      nav_panel("A", "Page A content"),
-      nav_panel("B", "Page B content"),
-      nav_panel("C", "Page C content"),
+      nav_panel("CS for All"),
+      nav_panel("CS Degree Trends in the U.S."),
+      nav_panel("CS Degrees by State"),
+      nav_panel("National AP CS Participation"),
       
       nav_menu(
-        "Other links",
-        nav_panel("D", "Panel D content"),
+        "About",
+        h3("About This Project"),
+        p("Created by [Michelle Cui]."),
+        p(
+          a("GitHub Repository",
+            href = "https://github.com/Go-M-C/Student_Access_to_AP_CS",
+            target = "_blank")
+        ),
         "----",
-        "Description:",
         nav_item(
           a("Shiny", href = "http://shiny.posit.co", target = "_blank")
         )
