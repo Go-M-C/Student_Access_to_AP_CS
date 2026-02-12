b6159ed8958c60d8a82c98072b9ce74cae4c39ac
Michelle <colaamigo@hotmail.com>
2026-02-11 21:33:08 -0800

    Data cleaning _ 2012-13

--- script/cs_ba_national.R
+++ script/cs_ba_national.R
@@ -16,10 +16,14 @@ cs_2012 <- read_excel(here("data","conferred degree_national_12_13.xls"),
                       skip = 1, 
                       col_names = TRUE) %>% 
   clean_names() %>% 
-  filter(
-    !str_detect(state_or_jurisdiction, regex("United States|Other jurisdictions", ignore_case = TRUE)),
-    str_detect(state_or_jurisdiction, "[A-Za-z]")
-  ) %>% 
+  
+  filter(!is.na(state_or_jurisdiction)) %>% 
+  filter(str_detect(state_or_jurisdiction, "[A-Za-z]")) %>% 
+  filter(!str_detect(state_or_jurisdiction, 
+                     regex("United States|Other jurisdictions", 
+                           ignore_case = TRUE))) %>% 
+  filter(!is.na(computer_sciences)) %>% 
+
   select(state_or_jurisdiction, computer_sciences) %>% 
   rename(
     state = state_or_jurisdiction,
@@ -27,9 +31,10 @@ cs_2012 <- read_excel(here("data","conferred degree_national_12_13.xls"),
   ) %>% 
   mutate(
     cs_ba_dgrees = as.numeric(str_remove_all(cs_ba_dgrees, ",")),
-    academic_year = "2012-13"
-  ) %>% 
-  filter(!cs_ba_dgrees %in% NA)
+    academic_year = "2012-13",
+    state = str_replace(state, "[^A-Za-z]+$",""),# remove any non-letter cha at end
+    state = str_squish(state) # remove extra space
+  )
    
 
 
