60c66e4ae88e062ec4c5e664c22de440ee551ca8
Michelle <colaamigo@hotmail.com>
2026-02-11 20:46:29 -0800

    NCES data cleaning_2012-13

---
+++ script/cs_ba_national.R
@@ -0,0 +1,40 @@
+library(tidyr)
+library(here)
+library(tidyverse)
+library(readxl)
+library(gganimate)
+library(gifski)
+library(janitor)
+
+
+# Data clean and wrangling
+
+## Import NCES data for 
+## conferred bachelor degree by postsecondary institutions
+
+cs_2012 <- read_excel(here("data","conferred degree_national_12_13.xls"), 
+                      skip = 1, 
+                      col_names = TRUE) %>% 
+  clean_names() %>% 
+  filter(
+    !str_detect(state_or_jurisdiction, regex("United States|Other jurisdictions", ignore_case = TRUE)),
+    str_detect(state_or_jurisdiction, "[A-Za-z]")
+  ) %>% 
+  select(state_or_jurisdiction, computer_sciences) %>% 
+  rename(
+    state = state_or_jurisdiction,
+    cs_ba_dgrees = computer_sciences
+  ) %>% 
+  mutate(
+    cs_ba_dgrees = as.numeric(str_remove_all(cs_ba_dgrees, ",")),
+    academic_year = "2012-13"
+  ) %>% 
+  filter(!cs_ba_dgrees %in% NA)
+   
+
+
+
+
+
+
+
