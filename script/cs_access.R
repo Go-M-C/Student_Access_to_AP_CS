d76600affc7baf21f1313b96bcdeef405ecc7c08
Michelle <colaamigo@hotmail.com>
2026-02-25 18:35:10 -0800

    Data cleaning for data that related to the CS(including AP CS) access in Oregon

--- script/cs_access.R
+++ script/cs_access.R
@@ -4,16 +4,52 @@ library(janitor)
 library(here)
 library(sf)
 library(plotly)
+library(readr)
+library(dplyr)
 # read in 20-21 enrollment data from ODE
 # read in 20-21 Oregon AP course count data from CRDC
 
-
-or_fall_202021 <- read_xlsx(here("data","fallmembershipreport_20202021.xlsx"),
-                          sheet = 4) %>% 
+# 1. CRDC AP ESSA DATA(2020-21)
+or_ap_raw<- read_xlsx(here("data","ESSA_202021_CRDC_AP_IB_DE.xlsx"),
+                          sheet = 2) %>% 
   clean_names() %>% 
+  mutate(across(starts_with("total_count_"), ~ na_if(., "*") %>% as.numeric()))
+
+or_ap_cs_prepared <- or_ap_raw %>% 
+  filter(course == "AP Computer Science") %>% 
+  mutate(
+    school_match = str_to_lower(str_trim(school_name)),
+    district_match = str_to_lower(str_trim(district_name))
+  ) %>% 
+  select(school_match, 
+         district_match, 
+         school_name, 
+         district_name,
+         cs_ap_total = total_count_all_students,
+         cs_ap_count_american_indian = total_count_american_indian_or_alaska_native,
+         cs_ap_count_asian = total_count_asian,
+         cs_ap_count_black = total_count_black_or_african_american,
+         cs_ap_count_hispanic = total_count_hispanic_or_latino,
+         cs_ap_count_native_ha_pa_islander = total_count_native_hawaiian_or_pacific_islander,
+         cs_ap_count_multi_racial = total_count_two_or_more_races,
+         cs_ap_count_white = total_count_white,
+         cs_ap_count_student_with_disability = total_count_students_with_disabilities,
+         cs_ap_count_female = total_count_females,
+         cs_ap_count_male = total_count_males,
+         cs_ap_count_ell = total_count_english_learners)
+
+# 2. ODE Fall Membership (2020-21)
+or_fall_raw <- read_xlsx(here("data","fallmembershipreport_20202021.xlsx"),sheet = 4) %>% 
+  clean_names() 
+
+or_fall_subset <- or_fall_raw %>% 
+  mutate(
+    school_match = str_to_lower(str_trim(school)),
+    district_match = str_to_lower(str_trim(district_name))
+  ) %>% 
   select(
+    school_match,district_match,
     district_id = attending_district_institution_id,
-    district_name,
     school_id = attending_school_institution_id,
     school_name = school,
     total_enrollment_202021 = x2020_21_total_enrollment,
@@ -37,65 +73,57 @@ or_fall_202021 <- read_xlsx(here("data","fallmembershipreport_20202021.xlsx"),
     grade_ten = x2020_21_grade_ten,
     grade_eleven = x2020_21_grade_eleven,
     grade_twelve = x2020_21_grade_twelve
-  ) %>% 
-  mutate(
-    school_id = as.character(school_id)
   )
+
+or_fall_enrollment <- or_fall_subset %>% 
+  select(
+    school_match,
+    district_match,
+    total_enrollment_202021
+  )
+# 3. CrseMap-OR (2021-22)
+  or_crsemap_2122 <- read_tsv(here("data","CrseMap-OR_Full Data_data.csv"),
+                              locale = locale(encoding = "UTF-16")) %>% 
+  clean_names()
+  
+  or_geo_subset <- or_crsemap_2122 %>% 
+    mutate(
+      school_match = str_to_lower(str_trim(campusname)),
+      district_match = str_to_lower(str_trim(districtname))
+    ) %>% 
+    dplyr::group_by(school_match, district_match) %>% 
+    dplyr::summarise(
+      lat = dplyr::first(latcod),
+      lon = dplyr::first(loncod),
+      locale = dplyr::first(locale),
+      total_cs_capacity = sum(as.numeric(number_of_courses), na.rm = TRUE),
+      subcategory_list = paste(unique(subcategory), collapse = ", "),
+      unique_course_types = n_distinct(subcategory)
+    ) %>% 
+    dplyr::ungroup()
+
+  
+# Join
   
 
-
-or_ap_202021 <- read_xlsx(here("data","ESSA_202021_CRDC_AP_IB_DE.xlsx"),
-                        sheet = 2) %>% 
-  clean_names() %>% 
-  mutate(across(starts_with("total_count_"), ~ na_if(., "*") %>% as.numeric())) %>% 
-  mutate(school_id = as.character(school_id_nces)) 
-
-or_ap_cs_202021 <- or_ap_202021 %>% 
-  filter(course == "AP Computer Science") %>% 
-  select(school_id,
-         cs_all_count_students = total_count_all_students,
-         cs_count_american_indian = total_count_american_indian_or_alaska_native,
-         cs_count_asin = total_count_asian,
-         cs_count_black = total_count_black_or_african_american,
-         cs_count_hispanic = total_count_hispanic_or_latino,
-         cs_count_native_ha_pa_islander = total_count_native_hawaiian_or_pacific_islander,
-         cs_count_multi_racial = total_count_two_or_more_races,
-         cs_count_white = total_count_white,
-         cs_count_student_with_disability = total_count_students_with_disabilities,
-         cs_count_female = total_count_females,
-         cs_count_male = total_count_males,
-         cs_count_ell = total_count_english_learners)
-
-
-or_apcs_capacity <- or_fall_202021 %>% 
-  left_join(or_ap_cs_202021, by = "school_id") %>% 
-  mutate(ap_cs_offered = if_else(!is.na(cs_all_count_students), 1,0))
-
-
-or_apcs_capacity <- or_apcs_capacity %>% 
-  mutate(
-    school_type = case_when(
-      grepl("elementary", school_name, ignore.case = TRUE) ~ "Elementary",
-      grepl("middle", school_name, ignore.case = TRUE) ~ "Middle",
-      grepl("high", school_name, ignore.case = TRUE) ~ "High",
-      grepl("charter", school_name, ignore.case = TRUE) ~ "Charter",
-      grepl("international", school_name, ignore.case = TRUE) ~ "International",
-      grepl("online", school_name, ignore.case = TRUE) ~ "Online",
-      TRUE ~ "Other"
+  apcs_fall_join <- or_ap_cs_prepared %>% 
+    left_join(or_fall_enrollment, by = c("school_match", "district_match"))
+  # match_summary <- test_join %>% 
+  #   summarise(
+  #     total_schools = n(),
+  #     matched_enrollment = sum(!is.na(total_enrollment_202021)),
+  #     missing_enrollment = sum(is.na(total_enrollment_202021))
+  #   )
+  
+  or_final_ecosystem <- apcs_fall_join %>% 
+    left_join(or_geo_subset, by = c("school_match","district_match")) %>% 
+    mutate(
+      total_cs_capacity = replace_na(total_cs_capacity, 0),
+      unique_course_types = replace_na(unique_course_types, 0)
     )
-  )
+  #cat("Schools with Map Coordinates:", sum(!is.na(or_final_ecosystem$lat)), "\n")
 
 
-capacity_summary <- or_apcs_capacity %>% 
-  group_by(school_type) %>% 
-  summarise(
-    total_schools = n(),
-    schools_offering_cs = sum(ap_cs_offered, na.rm = TRUE),
-    pct_offering_cs = round(schools_offering_cs/total_schools * 100, 1)
-  )
-capacity_summary
-
-
-
+or_final_ecosystem
 
 
